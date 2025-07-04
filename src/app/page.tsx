f1ef3583c90721d93307ad29ea9fb61e04010e95
savi727 <savinder.s@communicationcrafts.com>
2025-07-04 12:59:48 +0530

    add resetting database

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,10 +1,9 @@
 import prisma from "@/lib/db";
 
 export default async function Home() {
-  const users = await prisma.user.findMany()
   return (
     <div>
-      {JSON.stringify(users, null, 2)}
+      Hello world
     </div>
   )
 }
